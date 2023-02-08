884b77c16c8c76cebd7f62deebd22aaf963d3cae
Mohan Ram T.R <51279552+SpaceCoder123@users.noreply.github.com>
2023-02-08 08:34:36 +0530

    Add files via upload

---
+++ DSA/src/BubbleSort.java
@@ -0,0 +1,28 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = new int[]{ 5,8,6,3,22,31,1,11,21,42,22,0 };
+//        BubbleSort(arr);
+        int[] out = BubbleSort.BubbleS(arr);
+        for (int i = 0; i<out.length;i++){
+            System.out.print(out[i]+" ");
+        }
+    }
+    public static int[] BubbleS(int[] arr){
+
+        // sorting the array using swap mechanism
+        int n = arr.length;
+        for (int i = 0; i<n-1;i++){
+            for (int j = 0; j < n - i - 1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    // O(n^2) time complexity and therefore is stable but time consuming when compared to other sorting algorithms
+    // as the length of the array increases 
+}
